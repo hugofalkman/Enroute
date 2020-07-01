96afdbe2cb3fed06ee59d1efd51e8058357fdfef
hugofalkman <hugo@falkman.net>
2020-07-01 18:45:58 +0200

    Initial Commit

---
+++ Enroute/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Enroute
+//
+//  Created by H Hugo Falkman on 01/07/2020.
+//  Copyright © 2020 H Hugo Falkman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
